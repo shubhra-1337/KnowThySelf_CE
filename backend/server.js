c850ee2e9aded91b7f476be0cf708b9a4a7e709d
Shubhra Dabde <shubhra1337@gmail.com>
2026-04-15 23:14:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   "http://localhost:5500",
   "http://localhost:5000",
   "http://127.0.0.1:5500",
-  "https://knowthyself-7.vercel.app",
+  "know-thy-self-ce-frontend.vercel.app",
 ];
 app.use(
   cors({
